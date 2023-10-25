6338ff431a6486f6e28d5e8eeec0f5d203269243
k2maan <ketuman333@gmail.com>
2023-10-25 12:06:50 +0530

    Implemented: user details header on user details page (#3)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
+import UserDetails from '@/views/UserDetails.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/user-details/:partyId',
+    name: 'UserDetails',
+    component: UserDetails,
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/UserDetails.vue
@@ -0,0 +1,69 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-menu-button slot="start" />
+        <ion-title>{{ $t("User details") }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <main>
+        <ion-item lines="none">
+          <!-- TODO show image only if available -->
+          <ion-avatar slot="start">
+            <Image />
+          </ion-avatar>
+          <ion-label>
+            <h1>First Last</h1>
+            <p>username</p>
+          </ion-label>
+        </ion-item>
+      </main>
+    </ion-content>
+  </ion-page>
+</template>
+<script>
+import {
+  IonAvatar,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonMenuButton,
+  IonPage,
+  IonToolbar,
+  IonTitle,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useRouter } from 'vue-router';
+import { useStore } from "vuex";
+import { addOutline, arrowForwardOutline } from 'ionicons/icons';
+import Image from '@/components/Image.vue'
+
+export default defineComponent({
+  name: "User Details",
+  components: {
+    Image,
+    IonAvatar,
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonMenuButton,
+    IonPage,
+    IonToolbar,
+    IonTitle,
+  },
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+    return {
+      addOutline,
+      arrowForwardOutline,
+      router,
+      store
+    }
+  }
+})
+</script>
