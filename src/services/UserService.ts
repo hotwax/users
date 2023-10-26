a9e98e73a03e2003cf33f87b43acf8d3070200d5
amansinghbais <singh11amanbais@gmail.com>
2023-10-26 15:54:39 +0530

    Improved: removed unused entities (#2)

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,4 @@
 import { api, client } from '@/adapter'
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyTG9naW5JZCI6InN1bWl0aS5qb3NoaSIsImlzcyI6IkhvdFdheCIsImV4cCI6MTY5ODM4NDA5MSwiaWF0IjoxNjk4Mjk3NjkxfQ.fyjNEfv6qOysbc_okstRF39y2ok1jvxBLt3b0rH7R4QlVITve0lcKa-QmaREy49zDmVYOxfkJ3DewOuQ7qqyuA'
 
 const getPartyViewDetail = async (params: any): Promise<any> => {
   return client({
