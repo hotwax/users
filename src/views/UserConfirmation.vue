9395909a0227be7f6380be70b28cc1f145ee7a57
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2023-10-30 11:56:42 +0530

    Implemented: Added User confirmation page.

---
+++ src/views/UserConfirmation.vue
@@ -0,0 +1,115 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-back-button default-href="/create-user" slot="start"></ion-back-button>
+        <ion-title>{{ translate("Create user") }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-card>
+        <ion-item lines="none">
+          <ion-label v-if="selectedUser.groupName">{{ selectedUser.groupName }} </ion-label> 
+          <ion-label v-else>{{ selectedUser.firstName }} {{ selectedUser.lastName }}</ion-label>
+          <ion-note slot="end">{{ selectedUser.partyId }}</ion-note> 
+        </ion-item>
+        <ion-item v-if="selectedUser.emailDetails?.email">
+          <ion-icon :icon="mailOutline" slot="start" />
+          <ion-label>{{ selectedUser.emailDetails?.email}}</ion-label> 
+        </ion-item>
+        <ion-item v-if="selectedUser.phoneNumberDetails?.contactNumber">
+          <ion-icon :icon="callOutline" slot="start"/>
+          <ion-label>{{ selectedUser.phoneNumberDetails?.contactNumber }}</ion-label> 
+        </ion-item>
+        <ion-item v-if="selectedUser.externalId && selectedUser.partyTypeId !== 'PARTY_GROUP'">
+          <ion-icon :icon="businessOutline" slot="start"/>
+          <ion-label>{{ selectedUser.externalId }}</ion-label> 
+        </ion-item>
+      </ion-card>
+      <div class="ion-padding-top">
+        <ion-button @click="quickSetup()">
+          <ion-icon slot="end" :icon="arrowForwardOutline"/>
+          {{ translate("Quick Setup") }}
+        </ion-button>
+        <ion-button color="medium" fill="outline" @click="setupManually()">
+          <ion-icon slot="end" :icon="arrowForwardOutline"/>
+          {{ translate("Setup Manually") }}
+        </ion-button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+  
+<script lang="ts">
+  import {
+    IonBackButton,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonNote,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  } from "@ionic/vue";
+  import { defineComponent } from "vue";
+  import { mapGetters, useStore } from "vuex";
+  import { useRouter } from 'vue-router'
+  import {
+    arrowForwardOutline,
+    businessOutline,
+    callOutline,
+    mailOutline
+  } from 'ionicons/icons';
+  import { translate } from "@hotwax/dxp-components";
+  
+  export default defineComponent({
+    name: "UserConfirmation",
+    components: {
+      IonBackButton,
+      IonCard,
+      IonContent,
+      IonHeader,
+      IonIcon,
+      IonItem,
+      IonLabel,
+      IonNote,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+    },
+    computed: {
+      ...mapGetters({
+        selectedUser: 'user/getSelectedUser'
+      })
+    },
+    props: ['partyId'],
+    async ionViewWillEnter() {
+      await this.store.dispatch("user/getSelectedUserDetails", { partyId: this.partyId });
+    },
+    methods: {
+      async quickSetup() {
+        await this.$router.push({ path: `/user-quick-setup/${this.partyId}` })
+      },
+      async setupManually() {
+        await this.$router.push({ path: `/user-details/${this.partyId}` })
+      }
+    },
+    setup() {
+      const store = useStore();
+      const router = useRouter();
+  
+      return {
+        store,
+        router,
+        arrowForwardOutline,
+        businessOutline,
+        callOutline,
+        mailOutline,
+        translate
+      };
+    }
+  });
+</script>
