392b7949bde7aa6328f3703f905ba9b9d1febcff
Yash Maheshwari <yash.maheshwari@hotwax.co>
2024-04-19 15:46:03 +0530

    Fixed: the case where loading label is not displayed when infinite scrolling(#84)

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -130,7 +130,7 @@
       <ion-infinite-scroll
         @ionInfinite="loadMoreUsers($event)"
         threshold="100px"
-        v-show="isScrollingEnabled && isScrollable"
+        v-show="isScrollable"
         ref="infiniteScrollRef"
       >
         <ion-infinite-scroll-content
@@ -283,6 +283,10 @@ export default defineComponent({
       this.router.push({ path: `/user-details/${user.partyId}` })
     },
     async loadMoreUsers(event: any) {
+      // Added this check here as if added on infinite-scroll component the Loading content does not gets displayed
+      if(!(this.isScrollingEnabled && this.isScrollable)) {
+        await event.target.complete();
+      }
       this.fetchUsers(
         undefined,
         Math.ceil(
