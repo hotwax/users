a56031e1ba8d95499313065a0fe3fabef9985881
amansinghbais <singh11amanbais@gmail.com>
2024-04-24 14:43:59 +0530

    Improved: app level permission name for security permission create (#227)

--- src/components/PermissionItems.vue
+++ src/components/PermissionItems.vue
@@ -35,7 +35,7 @@
               <ion-card-title>{{ permission.permissionId }}</ion-card-title>
               <ion-card-subtitle>{{ permission.description }}</ion-card-subtitle>
             </div>
-            <ion-checkbox :disabled="permission.isChecked ? !hasPermission(Actions.APP_PERMISSION_UPDATE) : !hasPermission(Actions.APP_PERMISSION_ADD)" :checked="permission.isChecked" @click="updatePermissionAssociation($event, permission)" />
+            <ion-checkbox :disabled="permission.isChecked ? !hasPermission(Actions.APP_PERMISSION_UPDATE) : !hasPermission(Actions.APP_PERMISSION_CREATE)" :checked="permission.isChecked" @click="updatePermissionAssociation($event, permission)" />
           </ion-card-header>
         </ion-card>
       </section>
