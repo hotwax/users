49f14de04536eb77bffbdb956e4d9082ebd4fd5b
R-Sourabh <rsourabh002@gmail.com>
2025-04-11 11:18:45 +0530

    Fixed: Added error handling in the Security Groups API calls (#325)

--- src/views/UserDetails.vue
+++ src/views/UserDetails.vue
@@ -961,28 +961,33 @@ export default defineComponent({
           const securityGroupsToCreate = result.data.value.securityGroupsToCreate
           const securityGroupsToRemove = result.data.value.securityGroupsToRemove
 
-          const updateResponses = await Promise.allSettled(securityGroupsToRemove
-            .map(async (payload: any) => await UserService.removeUserSecurityGroup({
-              groupId: payload.groupId,
+          try {
+            const updateResponses = await Promise.allSettled(securityGroupsToRemove
+              .map(async (payload: any) => await UserService.removeUserSecurityGroup({
+                groupId: payload.groupId,
+                userLoginId: this.selectedUser.userLoginId
+              }))
+            )
+
+            const createResponse = await UserService.addUserToSecurityGroup({
+              groupIds: securityGroupsToCreate?.map((group: any) => group.groupId),
               userLoginId: this.selectedUser.userLoginId
-            }))
-          )
+            })
 
-          const createResponse = await UserService.addUserToSecurityGroup({
-            groupIds: securityGroupsToCreate?.map((group: any) => group.groupId),
-            userLoginId: this.selectedUser.userLoginId
-          })
-
-          const hasFailedResponse = [...updateResponses, createResponse].some((response: any) => response.status === 'rejected')
-          if (hasFailedResponse) {
+            const hasFailedResponse = [...updateResponses, createResponse].some((response: any) => response.status === 'rejected')
+            if (hasFailedResponse) {
+              showToast(translate('Failed to update some security group(s).'))
+            } else {
+              showToast(translate('Security group(s) updated successfully.'))
+            }
+            // refetching security groups
+            const userSecurityGroups = await UserService.getUserSecurityGroups(this.selectedUser.userLoginId)
+            this.store.dispatch('user/updateSelectedUser', { ...this.selectedUser, securityGroups: userSecurityGroups })
+            this.isUserFulfillmentAdmin = userSecurityGroups.length ? await UserService.isUserFulfillmentAdmin(userSecurityGroups.map((group: any) => group.groupId)) : false
+          } catch (error) {
+            logger.error(error)
             showToast(translate('Failed to update some security group(s).'))
-          } else {
-            showToast(translate('Security group(s) updated successfully.'))
           }
-          // refetching security groups
-          const userSecurityGroups = await UserService.getUserSecurityGroups(this.selectedUser.userLoginId)
-          this.store.dispatch('user/updateSelectedUser', { ...this.selectedUser, securityGroups: userSecurityGroups })
-          this.isUserFulfillmentAdmin = userSecurityGroups.length ? await UserService.isUserFulfillmentAdmin(userSecurityGroups.map((group: any) => group.groupId)) : false
         }
       })
 
