2b5be1aa51790f37118f77c422af5de061dc396d
Nirvan-Bobde30 <nirvan.bobde@hotwax.co>
2025-11-10 12:43:44 +0530

    Improved : upgraded apps-theme-package to v1.4.0 (dxp-438)

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -193,10 +193,4 @@ export default defineComponent({
   ion-content {
     --padding-bottom: 80px;
   }
-  /* Added conditional hiding in standalone mode that respects user permissions */
-  @media (display-mode: standalone) {
-    [standalone-hidden] {
-      display: none;
-    }
-  }
 </style>
